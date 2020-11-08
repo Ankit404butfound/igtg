8dcfbe50ea915289e51bf15fff76b6012c217171
Ankit Raj Mahapatra <54436840+Ankit404butfound@users.noreply.github.com>
2020-11-08 20:20:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ password = os.environ.get("password")
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("user/data/dir=selenium")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
@@ -35,6 +36,7 @@ def post(bot,update):
             time.sleep(2)
             driver.find_element_by_xpath("//*[@type='button']").click()
             count = 11
+            driver.get(url)
         
         except:
             count+=1
